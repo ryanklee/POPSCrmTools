a9e68278cce8454cfebbc43d363fc5dc40ede0b1
Ryan Kleeberger <rylklee@gmail.com>
2019-02-08 15:40:16 -0600

    Added comment based help. Changed param type to switch.

--- POPSCrmTools/Public/New-CrmSolutionFromSourceConfig.ps1
+++ POPSCrmTools/Public/New-CrmSolutionFromSourceConfig.ps1
@@ -1,20 +1,40 @@
 Function New-CrmSolutionFromSourceConfig {
+    <#
+        .SYNOPSIS
+            Generates config file for 'New-CrmSolutionFromSource'
+         
+        .DESCRIPTION
+            Generates config file for 'New-CrmSolutionFromSource'. Assumes
+            that all publisher info and solution name are the same for source
+            and target.
+        
+        .OUTPUTS
+            Psd1 config file of name specified in parameter.
+    #>
     Param
     (
+        # FileName for config
         [Parameter(Mandatory=$true)]
         [string]$FileName,
+        # Name of the Dynamics Crm org containing source Solution
         [Parameter(Mandatory=$true)]
         [string]$SourceOrg,
+        # Name of the Dynamics Crm org to contain target Solution
         [Parameter(Mandatory=$true)]
         [string]$TargetOrg,
+        # UniqueName of source Solution and for target solution
         [Parameter(Mandatory=$true)]
         [string]$SolutionName,
+        # Change any rootcomponentbehaviors of 0 to 1. Default is False.
         [Parameter(Mandatory=$true)]
-        [bool]$FixRootComponentBehavior,
+        [switch]$FixRootComponentBehavior = $false,
+        # UniqueName of Publisher
         [Parameter(Mandatory=$true)]
         [string]$PubName,
+        # DisplayName of Publisher
         [Parameter(Mandatory=$true)]
         [string]$PubDisplayName,
+        # Prefix for publisher
         [Parameter(Mandatory=$true)]
         [string]$PubPrefix
     )
